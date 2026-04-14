bb2c98a63c6f645c55981dd7342e04e60c530571
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-14 09:32:48 -0400

    stopped at line 15 commit

--- src/test/java/com/pluralsight/HighscoreWins.java
+++ src/test/java/com/pluralsight/HighscoreWins.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class HighscoreWins {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a game score: ");
+        String scoreline = input.nextLine();
+
+        String [] parts = scoreline.split("\\|");
+        String teamsPart = parts[0];
+        String scoresPart = parts[1];
+
+
+    }
 }
